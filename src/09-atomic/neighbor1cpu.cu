5e6387b4c1e68160972a14a22fd1f81328f44fe2
ZouJiu <1069679911@qq.com>
2023-09-08 14:31:40 +0800

    comment

--- src/09-atomic/neighbor1cpu.cu
+++ src/09-atomic/neighbor1cpu.cu
@@ -5,110 +5,110 @@
 #include <sstream>
 #include <string>
 #include <vector>
-
 #ifdef USE_DP
     typedef double real;
 #else
     typedef float real;
 #endif
 
-int N; // number of atoms
-const int NUM_REPEATS = 20; // number of timings
-const int MN = 10; // maximum number of neighbors for each atom
-const real cutoff = 1.9; // in units of Angstrom
-const real cutoff_square = cutoff * cutoff;
+int N; // number of atoms                  //atoms的个数
+const int NUM_REPEATS = 20; // number of timings    重复运算耗时的次数
+const int MN = 10; // maximum number of neighbors for each atom      //邻居的最大个数
+const real cutoff = 1.9; // in units of Angstrom         // 距离的最大值
+const real cutoff_square = cutoff * cutoff;   // 距离的平方
 
-void read_xy(std::vector<real>& x, std::vector<real>& y);
-void timing(int *NN, int *NL, std::vector<real> x, std::vector<real> y);
-void print_neighbor(const int *NN, const int *NL);
-
+void read_xy(std::vector<real>& x, std::vector<real>& y);    //从x, y坐标的txt档案读取坐标
+void timing(int *NN, int *NL, std::vector<real> x, std::vector<real> y);  //耗时
+void print_neighbor(const int *NN, const int *NL); //输出到txt档案
+ 
 int main(void)
 {
-    std::vector<real> x, y;
-    read_xy(x, y);
-    N = x.size();
-    int *NN = (int*) malloc(N * sizeof(int));
-    int *NL = (int*) malloc(N * MN * sizeof(int));
+    std::vector<real> x, y;    //初始化vector向量数组的
+    read_xy(x, y);             //读取
+    N = x.size();              //atoms的个数
+    int *NN = (int*) malloc(N * sizeof(int));   //分配atoms个数个内存
+    int *NL = (int*) malloc(N * MN * sizeof(int)); //分配矩阵内存，每个碳atoms分配最大邻居10个
     
-    timing(NN, NL, x, y);
-    print_neighbor(NN, NL);
+    timing(NN, NL, x, y);   // 运算邻居的呢
+    print_neighbor(NN, NL);   //  输出邻居的呢
 
-    free(NN);
-    free(NL);
+    free(NN);  //释放内存的
+    free(NL);  //释放内存的
     return 0;
 }
 
-void read_xy(std::vector<real>& v_x, std::vector<real>& v_y)
+void read_xy(std::vector<real>& v_x, std::vector<real>& v_y)   // 参数是x, y方向的坐标
 {
-    std::ifstream infile("xy.txt");
-    std::string line, word;
-    if(!infile)
+    std::ifstream infile("xy.txt");          // 读取输入坐标x,y档案
+    std::string line, word;    //初始化 string 字符串
+    if(!infile)    // 文件是否正常open
     {
-        std::cout << "Cannot open xy.txt" << std::endl;
+        std::cout << "Cannot open xy.txt" << std::endl;     //文件open出错了
         exit(1);
     }
-    while (std::getline(infile, line))
+    while (std::getline(infile, line))   //  从读取的文件内拿到一行
     {
-        std::istringstream words(line);
-        if(line.length() == 0)
+        std::istringstream words(line);     //  从这一行内读取字符串
+        if(line.length() == 0)   //是否空
         {
             continue;
         }
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 2; i++)   //两个x，y坐标的
         {
-            if(words >> word)
-            {
+            if(words >> word)   // 字符串依次赋值给word
+            { 
                 if(i == 0)
                 {
-                    v_x.push_back(std::stod(word));
+                    v_x.push_back(std::stod(word));   // 首先是x坐标
                 }
                 if(i==1)
                 {
-                    v_y.push_back(std::stod(word));
+                    v_y.push_back(std::stod(word)); //  然后是y坐标
                 }
             }
             else
             {
-                std::cout << "Error for reading xy.txt" << std::endl;
+                std::cout << "Error for reading xy.txt" << std::endl;  //读取出错的
                 exit(1);
             }
         }
     }
-    infile.close();
+    infile.close();  //关闭打开的文件
 }
 
-void find_neighbor(int *NN, int *NL, const real* x, const real* y)
+// 参数是 NN数组 N个碳atoms，NL邻居列表的 NxMN也就是Nx10，x是x坐标，y是y坐标
+void find_neighbor(int *NN, int *NL, const real* x, const real* y)  
 {
-    for (int n = 0; n < N; n++)
+    for (int n = 0; n < N; n++)   
     {
-        NN[n] = 0;
+        NN[n] = 0;     //  N个碳atoms的邻居个数初始化到 0
     }
 
-    for (int n1 = 0; n1 < N; ++n1)
+    for (int n1 = 0; n1 < N; ++n1)  //遍历 N 个碳atoms
     {
-        real x1 = x[n1];
-        real y1 = y[n1];
-        for (int n2 = n1 + 1; n2 < N; ++n2)
+        real x1 = x[n1];  // 第n1个碳atoms的x坐标
+        real y1 = y[n1];  // 第n1个碳atoms的y坐标
+        for (int n2 = n1 + 1; n2 < N; ++n2)        //遍历其他的碳atoms，因n1, n2或者n2, n1是相同的内容，所以截断一半
         {
-            real x12 = x[n2] - x1;
-            real y12 = y[n2] - y1;
-            real distance_square = x12 * x12 + y12 * y12;
-            if (distance_square < cutoff_square)
+            real x12 = x[n2] - x1; // n1和n2的x坐标相减
+            real y12 = y[n2] - y1; // n1和n2的y坐标相减
+            real distance_square = x12 * x12 + y12 * y12;  // 求出坐标差的平方和
+            if (distance_square < cutoff_square)       //若是平方和 < 给定的阀值，则算邻居的
             {
-                NL[n1 * MN + NN[n1]++] = n2;
-                NL[n2 * MN + NN[n2]++] = n1;
+                NL[n1 * MN + NN[n1]++] = n2;//  record邻居的index，NL可以看作矩阵，n1是行，NN[n1]是列也是n1的邻居个数
+                NL[n2 * MN + NN[n2]++] = n1;//  record邻居的index，NL可以看作矩阵，n2是行，NN[n2]是列也是n2的邻居个数
             }
         }
     }
 }
 
-void timing(int *NN, int *NL, std::vector<real> x, std::vector<real> y)
+void timing(int *NN, int *NL, std::vector<real> x, std::vector<real> y) // 耗时
 {
-    for (int repeat = 0; repeat < NUM_REPEATS; ++repeat)
+    for (int repeat = 0; repeat < NUM_REPEATS; ++repeat)  // 算耗时需要重复NUM_REPEATS次
     {
-        cudaEvent_t start, stop;
-        CHECK(cudaEventCreate(&start));
-        CHECK(cudaEventCreate(&stop));
+        cudaEvent_t start, stop;                  //  用来算耗时的
+        CHECK(cudaEventCreate(&start));           //  初始化
+        CHECK(cudaEventCreate(&stop));//  初始化
         CHECK(cudaEventRecord(start));
         while(cudaEventQuery(start)!=cudaSuccess){}
         find_neighbor(NN, NL, x.data(), y.data());
@@ -116,7 +116,7 @@ void timing(int *NN, int *NL, std::vector<real> x, std::vector<real> y)
         CHECK(cudaEventRecord(stop));
         CHECK(cudaEventSynchronize(stop));
         float elapsed_time;
-        CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+        CHECK(cudaEventElapsedTime(&elapsed_time, start, stop)); //  算耗时
         std::cout << "Time = " << elapsed_time << " ms." << std::endl;
 
         CHECK(cudaEventDestroy(start));
@@ -124,10 +124,10 @@ void timing(int *NN, int *NL, std::vector<real> x, std::vector<real> y)
     }
 }
 
-void print_neighbor(const int *NN, const int *NL)
+void print_neighbor(const int *NN, const int *NL)        // 输出到档案内部
 {
-    std::ofstream outfile("neighbor.txt");
-    if (!outfile)
+    std::ofstream outfile("neighbor.txt");               //  open输出档案
+    if (!outfile)                                        //  输出档案是否open
     {
         std::cout << "Cannot open neighbor.txt" << std::endl;
     }
@@ -138,20 +138,19 @@ void print_neighbor(const int *NN, const int *NL)
             std::cout << "Error: MN is too small." << std::endl;
             exit(1);
         }
-        outfile << NN[n];
-        for (int k = 0; k < MN; ++k)
+        outfile << NN[n];                       // 首先给出第n个碳atoms的邻居个数
+        for (int k = 0; k < MN; ++k)            //  遍历MN个邻居
         {
-            if(k < NN[n])
+            if(k < NN[n])                       //  k要满足 < 邻居的个数 NN[n]
             {
-                outfile << " " << NL[n * MN + k];
+                outfile << " " << NL[n * MN + k];    //  输出第n个碳atoms，第k个邻居的标号index，行n,列k
             }
             else
             {
-                outfile << " NaN";
+                outfile << " NaN";         // 没有这么多邻居，输出NaN
             }
         }
-        outfile << std::endl;
+        outfile << std::endl;   // 结束符
     }
-    outfile.close();
+    outfile.close();   //  关闭输出档案
 }
-
--- src/09-atomic/neighbor2gpu.cu
+++ src/09-atomic/neighbor2gpu.cu
@@ -26,7 +26,7 @@ void print_neighbor(const int *, const int *, const bool);
 int main(void)
 {
     std::vector<real> v_x, v_y;
-    read_xy(v_x, v_y);
+    read_xy(v_x, v_y);  //读取x,y坐标到向量内
     N = v_x.size();
     int mem1 = sizeof(int) * N;
     int mem2 = sizeof(int) * N * MN;
@@ -35,29 +35,29 @@ int main(void)
     int *h_NL = (int*) malloc(mem2);
     int *d_NN, *d_NL;
     real *d_x, *d_y;
-    CHECK(cudaMalloc(&d_NN, mem1));
-    CHECK(cudaMalloc(&d_NL, mem2));
-    CHECK(cudaMalloc(&d_x, mem3));
-    CHECK(cudaMalloc(&d_y, mem3));
-    CHECK(cudaMemcpy(d_x, v_x.data(), mem3, cudaMemcpyHostToDevice));
-    CHECK(cudaMemcpy(d_y, v_y.data(), mem3, cudaMemcpyHostToDevice));
+    CHECK(cudaMalloc(&d_NN, mem1)); // 分配GPU显存给 d_NN，用来 record 每个碳atoms邻居个数的
+    CHECK(cudaMalloc(&d_NL, mem2));  // 分配GPU显存该 d_NL，用来 record 每个碳atoms的邻居，可以看作矩阵的
+    CHECK(cudaMalloc(&d_x, mem3));   //  分配GPU显存给 d_x，用来存x坐标
+    CHECK(cudaMalloc(&d_y, mem3));   // 分配GPU显存给 d_y，用来存y坐标
+    CHECK(cudaMemcpy(d_x, v_x.data(), mem3, cudaMemcpyHostToDevice)); //  复制x到d_x
+    CHECK(cudaMemcpy(d_y, v_y.data(), mem3, cudaMemcpyHostToDevice));  //  复制y到d_y
 
     std::cout << std::endl << "using atomicAdd:" << std::endl;
-    timing(d_NN, d_NL, d_x, d_y, true);
+    timing(d_NN, d_NL, d_x, d_y, true);   //  耗时
     std::cout << std::endl << "not using atomicAdd:" << std::endl;
-    timing(d_NN, d_NL, d_x, d_y, false);
+    timing(d_NN, d_NL, d_x, d_y, false);  //  耗时
 
 
-    CHECK(cudaMemcpy(h_NN, d_NN, mem1, cudaMemcpyDeviceToHost));
-    CHECK(cudaMemcpy(h_NL, d_NL, mem2, cudaMemcpyDeviceToHost));
+    CHECK(cudaMemcpy(h_NN, d_NN, mem1, cudaMemcpyDeviceToHost)); // 复制到host h_NN
+    CHECK(cudaMemcpy(h_NL, d_NL, mem2, cudaMemcpyDeviceToHost)); // 复制到host h_NL
 
     print_neighbor(h_NN, h_NL, false);
 
-    CHECK(cudaFree(d_NN));
+    CHECK(cudaFree(d_NN)); //  释放显存的
     CHECK(cudaFree(d_NL));
-    CHECK(cudaFree(d_x));
+    CHECK(cudaFree(d_x)); 
     CHECK(cudaFree(d_y));
-    free(h_NN);
+    free(h_NN);           //  释放host内存的
     free(h_NL);
     return 0;
 }
@@ -101,57 +101,66 @@ void read_xy(std::vector<real>& v_x, std::vector<real>& v_y)
     infile.close();
 }
 
-void __global__ find_neighbor_atomic
+//  使用了atomic函数
+void __global__ find_neighbor_atomic  
 (
     int *d_NN, int *d_NL, const real *d_x, const real *d_y,
     const int N, const real cutoff_square
 )
 {
-    const int n1 = blockIdx.x * blockDim.x + threadIdx.x;
-    if (n1 < N)
+    const int n1 = blockIdx.x * blockDim.x + threadIdx.x; // 拿到线程的标号index，表示第n1个碳 atoms 所在线程的
+    if (n1 < N)  //  线程没有越界就可以
     {
-        d_NN[n1] = 0;
-        const real x1 = d_x[n1];
-        const real y1 = d_y[n1];
+        d_NN[n1] = 0;            //初始化，第n1个碳的邻居个数 = 0
+        const real x1 = d_x[n1];      // 第n1个碳atoms的x坐标
+        const real y1 = d_y[n1];      // 第n1个碳atoms的y坐标
         for (int n2 = n1 + 1; n2 < N; ++n2)
         {
-            const real x12 = d_x[n2] - x1;
-            const real y12 = d_y[n2] - y1;
-            const real distance_square = x12 * x12 + y12 * y12;
-            if (distance_square < cutoff_square)
+            const real x12 = d_x[n2] - x1;  // n1和n2的x坐标相减
+            const real y12 = d_y[n2] - y1;  // n1和n2的y坐标相减
+            const real distance_square = x12 * x12 + y12 * y12;    // 求出坐标差的平方和
+            if (distance_square < cutoff_square) //若是平方和 < 给定的阀值，则算邻居的
             {
+                //  record邻居的index，d_NL可以看作矩阵，n1是行，d_NN[n1]是列也是n1的邻居个数，atomicAdd等价 “++”自增操作的
                 d_NL[n1 * MN + atomicAdd(&d_NN[n1], 1)] = n2;
+   //  record邻居的index，d_NL可以看作矩阵，n2是行，d_NN[n2]是列也是n1的邻居个数，atomicAdd等价 “++”自增操作的
                 d_NL[n2 * MN + atomicAdd(&d_NN[n2], 1)] = n1;
             }
         }
     }
 }
 
+//  不使用atomic函数
 void __global__ find_neighbor_no_atomic
 (
     int *d_NN, int *d_NL, const real *d_x, const real *d_y,
     const int N, const real cutoff_square
 )
 {
-    const int n1 = blockIdx.x * blockDim.x + threadIdx.x;
+    // 这的 n1 可以看作是列，N 是碳atoms的个数
+    const int n1 = blockIdx.x * blockDim.x + threadIdx.x; // 拿到线程的标号index，表示第n1个碳 atoms 所在线程的
     if (n1 < N)
     {
-        int count = 0;
-        const real x1 = d_x[n1];
-        const real y1 = d_y[n1];
-        for (int n2 = 0; n2 < N; ++n2)
+        int count = 0;  // record邻居的个数
+        const real x1 = d_x[n1];  // 第n1个碳atoms的x坐标
+        const real y1 = d_y[n1];  // 第n1个碳atoms的y坐标
+        // n2 可以看作是行的，但保存数据用的是count，保证record是continuous的
+        for (int n2 = 0; n2 < N; ++n2)  //没有截断一半，而是从0开始，因不能使用atomic函数
         {
-            const real x12 = d_x[n2] - x1;
-            const real y12 = d_y[n2] - y1;
-            const real distance_square = x12 * x12 + y12 * y12;
-            if ((distance_square < cutoff_square) && (n2 != n1))
+            const real x12 = d_x[n2] - x1;  // n1和n2的x坐标相减
+            const real y12 = d_y[n2] - y1;  // n1和n2的y坐标相减
+            const real distance_square = x12 * x12 + y12 * y12;   // 求出坐标差的平方和
+            if ((distance_square < cutoff_square) && (n2 != n1)) //若是平方和 < 给定的阀值，则算邻居的，且不是同一个碳atoms
             {
+            // 相邻的threadIdx.x对应的线程的 d_x, d_y, d_NL也是相邻的，所以n1看作列；
+            // count看作行，n1 可以看作是列，相邻的n1也就是相邻的threadIdx.x对应的d_NL也是相邻的；
+            // 相邻的，所以写入操作是合并的；写入方向是矩阵的列
                 d_NL[(count++) * N + n1] = n2;
             }
         }
-        d_NN[n1] = count;
+        d_NN[n1] = count; // record 第 n1个碳的邻居个数
     }
-}  
+}
 
 void timing
 (
@@ -167,12 +176,12 @@ void timing
         CHECK(cudaEventRecord(start));
         cudaEventQuery(start);
 
-        int block_size = 128;
-        int grid_size = (N + block_size - 1) / block_size;
+        int block_size = 128; //block size = blockDim.x，一个block块内部线程的个数
+        int grid_size = (N + block_size - 1) / block_size; // grid size = gridDim.x，一个网格grid内部的block个数
 
         if (atomic)
         {
-            find_neighbor_atomic<<<grid_size, block_size>>>
+            find_neighbor_atomic<<<grid_size, block_size>>> // 写入执行配置的
             (d_NN, d_NL, d_x, d_y, N, cutoff_square);
         }
         else
